c90afa87fc11c8acd4b52eb9fb46406f4da86619
xsat <yurii.antonchyk@gmail.com>
2020-01-29 21:50:17 +0200

    Updated games controller

--- app/Controllers/V1/GamesController.php
+++ app/Controllers/V1/GamesController.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace App\Controllers\V1;
 
 use App\Controllers\AbstractController;
+use App\Exceptions\HttpNotFoundException;
+use App\Mappers\GameMapper;
+use App\Models\Game;
+use MongoDB\Client;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -17,34 +22,52 @@ class GamesController extends AbstractController
      */
     public function create(): Response
     {
-        return $this->json();
+        $game = new Game();
+        if (!(new GameMapper(new Client()))->create($game)) {
+            throw new RuntimeException('Game was not created.');
+        }
+
+        return $this->json($game);
     }
 
     /**
      * @param string $gameId
      *
      * @return Response
+     *
+     * @throws HttpNotFoundException
      */
     public function show(string $gameId): Response
     {
-        return $this->json(
-            [
-                'gameId' => $gameId,
-            ]
-        );
+        $game = (new GameMapper(new Client()))->findById($gameId);
+        if ($game === null) {
+            throw new HttpNotFoundException('Game was not found.');
+        }
+
+        return $this->json($game);
     }
 
     /**
      * @param string $gameId
      *
      * @return Response
+     *
+     * @throws HttpNotFoundException
+     * @throws RuntimeException
      */
     public function update(string $gameId): Response
     {
-        return $this->json(
-            [
-                'gameId' => $gameId,
-            ]
-        );
+        $gameMapper = new GameMapper(new Client());
+        $game = $gameMapper->findById($gameId);
+        if ($game === null) {
+            throw new HttpNotFoundException('Game was not found.');
+        }
+
+        $game->setEnded(true);
+        if (!$gameMapper->update($game)) {
+            throw new RuntimeException('Game was not updated.');
+        }
+
+        return $this->json($game);
     }
 }
--- app/Mappers/GameMapper.php
+++ app/Mappers/GameMapper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Mappers;
 
+use App\Models\Game;
+use MongoDB\BSON\ObjectId;
 use MongoDB\Client;
 
 /**
@@ -24,5 +26,76 @@ class GameMapper
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * @param Game $game
+     *
+     * @return bool
+     */
+    public function create(Game $game): bool
+    {
+        $result = $this->client->battle->games->insertOne(
+            $this->convert($game)
+        );
+
+        $game->setGameId((string)$result->getInsertedId());
+
+        return $game->getGameId() !== null;
+    }
+
+    /**
+     * @param Game $game
+     *
+     * @return bool
+     */
+    public function update(Game $game): bool
+    {
+        $result = $this->client->battle->games->updateOne(
+            ['_id' => new ObjectId($game->getGameId())],
+            ['$set' => $this->convert($game)]
+        );
+
+        return $result->getMatchedCount() != 0;
+    }
+
+    /**
+     * @param Game $game
+     *
+     * @return array
+     */
+    private function convert(Game $game): array
+    {
+        return [
+            'players' => [],
+            'rounds' => [],
+            'is_ended' => $game->isEnded(),
+            'date_started' => $game->getDateStarted(),
+            'date_ended' => $game->getDateEnded(),
+        ];
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return Game|null
+     */
+    public function findById(string $id): ?Game
+    {
+        $result = $this->client->battle->games->findOne(
+            ['_id' => new ObjectId($id)]
+        );
+
+        if (!$result) {
+            return null;
+        }
+
+        $game = new Game();
+        $game->setGameId((string)$result['_id']);
+        $game->setEnded($result['is_ended']);
+        $game->setDateStarted($result['date_started']);
+        $game->setDateEnded($result['date_ended']);
+
+        return $game;
     }
 }
